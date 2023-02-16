848f79f1a105b16eefcdd4460ad5b8ba2fdea246
cbs970524 <58094664+cbs970524@users.noreply.github.com>
2023-02-16 19:47:25 +0900

    study_class_1

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+    String title;
+    String writer;
+    int printedDay;
+    int page;
+    static int bookCount = 0;
+    int bookId;
+
+    {
+        bookId = ++bookCount;
+    }
+
+    Book(String title){
+        this.title = title;
+    }
+    Book(String title, String writer){
+        this.title = title;
+        this.writer = writer;
+    }
+
+
+    String takeBook(String title){
+        System.out.println(title + "을 대여합니다.");
+        return title;
+    }
+    String returnBook(String title){
+        System.out.println(title + "을 반납합니다.");
+        return title;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BroadcastTeam GMBS = new BroadcastTeam();
+        GMBS.addNewMember("최범식");
+        GMBS.addNewMember("홍길동");
+        GMBS.showMember();
+    }
+}
